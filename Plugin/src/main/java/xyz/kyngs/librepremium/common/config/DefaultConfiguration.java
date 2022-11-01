a6c5fedb5b157c644295fc21b338db26f61d15dd
kyngs <kyngs@users.noreply.github.com>
2022-11-01 19:31:11 +0100

    Clarify comments

--- Plugin/src/main/java/xyz/kyngs/librepremium/common/config/DefaultConfiguration.java
+++ Plugin/src/main/java/xyz/kyngs/librepremium/common/config/DefaultConfiguration.java
@@ -37,7 +37,7 @@ public class DefaultConfiguration {
             PASS_THROUGH_DEFAULT,
             """
                     The servers player should be sent to when they are authenticated. THE SERVERS MUST BE REGISTERED IN THE PROXY CONFIG.
-                    The configuration allows to configure forced hosts, the servers under "root" are used from the root domain. Use § instead of dots.
+                    The configuration allows configuring forced hosts; the servers are used when players do not connect from a forced host. Use § instead of dots.
                     See: https://github.com/kyngs/LibrePremium/wiki/Configuring-Forced-Hosts
                     """,
             ConfigurateHelper::getServerMap
