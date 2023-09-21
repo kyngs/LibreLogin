2d5848f6154bc8f8071111b63e95addbf52b76b1
kyngs <kyngs@users.noreply.github.com>
2023-09-21 18:17:19 +0200

    Update LogFilter

--- Plugin/src/main/java/xyz/kyngs/librelogin/common/log/LogFilter.java
+++ Plugin/src/main/java/xyz/kyngs/librelogin/common/log/LogFilter.java
@@ -28,6 +28,10 @@ public abstract class LogFilter {
         PROTECTED_COMMANDS.add("changepass");
         PROTECTED_COMMANDS.add("passch");
         PROTECTED_COMMANDS.add("passwd");
+        PROTECTED_COMMANDS.add("confirmpasswordreset");
+        PROTECTED_COMMANDS.add("setemail");
+        PROTECTED_COMMANDS.add("librelogin user register");
+        PROTECTED_COMMANDS.add("librelogin user pass-change");
     }
 
     protected boolean checkMessage(String message) {
